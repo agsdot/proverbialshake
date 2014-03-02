a9762960b6452fc029041729762b69aa7efa16a1
Jonathan Lai <jonathanlai@eltux.net>
2014-03-01 19:54:07 -0800

    chapter per day, verse shuffle upon refresh

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
+var request = require("request");
+var _ = require("underscore");
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+
+  var todaysDate = new Date().getDate().toString();
+  var uri2 = "http://api.biblia.com/v1/bible/content/kjv.json?passage=Proverbs" + todaysDate +
+  "&style=oneVersePerLineFullReference" +
+  "&key=" + process.env.REGISTERED_BIBLIA_KEY
+
+  request({
+    uri: uri2,
+    }, function(error, response, body) {
+    var verseArray = [];
+    var filteredArray = [];
+    var shuffledArray = [];
+
+    response  = JSON.parse(body);
+    verseArray = (response.text).split(/\r*\n*Proverbs \d*:\d* /)
+    filteredArray = _.without(verseArray, "");
+    shuffledArray = _.shuffle(filteredArray);
+
+    res.render('index', { title: 'The Proverbial Shake', verses: shuffledArray, todaysDate: todaysDate });
+
+  });
+
 };
+
